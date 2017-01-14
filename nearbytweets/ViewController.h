321ed17ad21bef87430e39192a73c74dec3f85f1
IAUGMENTOR <iaugmentor@IAUGMENTORs-MacBook-Pro.local>
2017-01-14 13:25:50 +0530

    Initial Commit

---
+++ nearbytweets/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  nearbytweets
+//
+//  Created by IAUGMENTOR on 14/01/17.
+//  Copyright © 2017 Vinay Krishna Gupta. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ nearbytweets/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  nearbytweets
+//
+//  Created by IAUGMENTOR on 14/01/17.
+//  Copyright © 2017 Vinay Krishna Gupta. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
